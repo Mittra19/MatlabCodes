4b14d0a40833559264f3c0f1612ac64ad694a055
Anubhav Mittra <anubhavmittra19@gmail.com>
2020-12-03 17:31:26 +0530

    Update SamplingTheorem.m

--- SamplingTheorem.m
+++ SamplingTheorem.m
@@ -4,11 +4,11 @@ T=1/f;                  %Time period of the input signal
 Tmin = 0;
 Tmax = 10*T;            % Plot 10 cycles
 fs = 200;               %sampling frequency
-dt = T/100;             %sampling interval
-dt1= 1/51;
+dt = T/100;             
+dt1= 1/fs;              %sampling interval
 t1 = Tmin:dt:Tmax;
 t2 = Tmin:dt1:Tmax; 
-y1 = Am*sin(2*pi*f*t1); %Input signal to be sampled
+y1 = Am*sin(2*pi*f*t1); %Input signal
 y2 = Am*sin(2*pi*f*t2); %Sampled signal
 plot(t1, y1, 'r');
 hold on
